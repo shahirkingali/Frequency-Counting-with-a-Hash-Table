fb04277a28fd80cfbfb1734346acd4576bd74dad
Shahir Ali <shahirsheikali@gmail.com>
2021-02-26 20:25:24 -0800

    push

--- HashTable.py
+++ HashTable.py
@@ -41,7 +41,7 @@ class HashTable:
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
+    
 
 
 
