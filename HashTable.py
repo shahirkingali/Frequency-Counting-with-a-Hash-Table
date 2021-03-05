1273019dde3e98605d18ebf9703e204abed16fc9
Shahir Ali <shahirsheikali@gmail.com>
2021-03-04 19:42:40 -0800

    push

--- HashTable.py
+++ HashTable.py
@@ -6,69 +6,52 @@ class HashTable:
     self.size = size
     self.arr = self.create_arr(size)
 
-
   # 1️⃣ TODO: Complete the create_arr method.
 
+  
   # Each element of the hash table (arr) is a linked list.
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
-  def create_arr(self, size):
+  def create_arr(self, size): 
     array = []
 
     for i in range(size):
       llist = LinkedList()
-      array.append(LinkedList)
+      array.append(llist)
 
     return array
 
+    # 2️⃣ TODO: Create your own hash function.
 
-
-
-  # 2️⃣ TODO: Create your own hash function.
-
-  # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
+    # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored.
 
   def hash_func(self, key):
-    
-    index = hash(key) % self.size
-    
-    return index
-
+        index = hash(key) % self.size
+        return index
 
   # 3️⃣ TODO: Complete the insert method.
 
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
-  def insert(self, key, value):
-    key_hash = self.hash_func(key)
+  def insert(self, key, value): 
+    hashKey = self.hash_func(key)
     item = (key, value)
-    
-    new_insert = self.arr[key_hash].find(key)
-    
-    if new_insert != -1:
-       new_list = self.arr[key_hash].append(new_insert)
-       self.arr[key_hash].replace(new_list)
-    else:
-       self.arr[key_hash].append(item)
+    llist = self.arr[hashKey].find(key)
 
-
-
+    if llist == -1:
+        self.arr[hashKey].append(item)
 
   # 4️⃣ TODO: Complete the print_key_values method.
 
   # Traverse through the every Linked List in the table and print the key value pairs.
 
-  # For example: 
-  # a: 1
-  # again: 1
-  # and: 1
-  # blooms: 1
-  # erase: 2
+    # For example:
+    # a: 1
+    # again: 1
+    # and: 1
+    # blooms: 1
+    # erase: 2
 
-  def print_key_values(self):
-    for LinkedList in self.arr:
-      LinkedList.print_nodes()
-
-  
-  print("Finished")
-
+  def print_key_values(self): 
+    for i in range(self.size):
+      self.arr[i].print_nodes()
